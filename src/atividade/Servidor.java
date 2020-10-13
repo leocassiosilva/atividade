25fda348660f1085cd397c1d82cab8ca4488c19f
leocassiosilva <leocassiosilva1234@gmail.com>
2020-10-13 20:43:50 -0300

    Servidor

--- src/atividade/Servidor.java
+++ src/atividade/Servidor.java
@@ -1,8 +1,20 @@
 package atividade;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Leocassio
  */
 public class Servidor {
-    
+    public static void main(String[] args) {
+        try (ServerSocket servidor = new ServerSocket(999)){
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
